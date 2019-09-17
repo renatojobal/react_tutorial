eea2100140cee31c51fb480459531523b63eb212
renatojobal <renatojobal@gmail.com>
2019-09-17 11:16:38 -0500

    isto para calcular el ganador

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,136 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// class Square extends React.Component {
+//   // constructor(props) {
+//   //   super(props);
+//   //   this.state = {
+//   //     value: null,
+//   //   }
+//   // }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+//   render() {
+//     return (
+//       <button 
+//       className="square" 
+//       onClick={() => {this.props.onCLick() }}
+//       >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+// REmplazo la calse Square por un compenete de funcion
+function Square(props){
+    return (
+      <button 
+      className="square" 
+      onClick={props.onCLick}
+      >
+        {props.value}
+      </button>
+    );
+
+}
+
+class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    // const squares = this.state['squares'].slice();
+    const squares = this.state.squares.slice();
+    // if(this.state.xIsNext){
+    //   squares[i] = 'X';
+    //   this.setState({
+    //     squares: squares,
+    //     xIsNext: false,
+    //   });             MI MANERA DE RESOLVERLO
+    // }else {
+    //   squares[i] = 'O';
+    //   this.setState({
+    //     squares: squares,
+    //     xIsNext: true,
+    //   });
+    // }
+
+
+    // Segun el tutorial
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+
+
+  }
+
+  renderSquare(i) {
+    return (
+    <Square 
+      value={this.state.squares[i]} 
+      onCLick={() => this.handleClick(i)}
+    />
+    );
+  }
+
+  render() {
+    const status = 'Next player: '+ (this.state.xIsNext ? 'X' : 'O');
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+// =======================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+
